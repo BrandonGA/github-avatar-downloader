e05bae1505e48fa2c234a349c8038fd0b2dcb9e5
Brandon Galea-Andrews <brandon.galeaandrews@gmail.com>
2017-03-14 17:11:35 +0000

    Step 5 Completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = 'BrandonGA';
@@ -22,7 +23,10 @@ var GITHUB_TOKEN = 'b5fdb84f8ea04bb86ce1a56a671e8c3fd3aaedce';
   request.get(options , function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received //
-    console.log('body:', body);
+    // console.log('body:', JSON.parse(body)[0].avatar_url);
+    JSON.parse(body).forEach(function(contributor) {
+      console.log(contributor.avatar_url)
+    });
   })
   console.log(options, requestURL)
 }
